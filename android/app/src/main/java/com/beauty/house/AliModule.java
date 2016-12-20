a8be9ce298cfa56ab352aaeebef5b0943ccffbe2
walker <walker.lwh@gmail.com>
2016-12-20 14:45:01 +0800

    third party integration

---
+++ android/app/src/main/java/com/beauty/house/AliModule.java
@@ -0,0 +1,39 @@
+package com.beauty.house;
+
+import com.alipay.sdk.app.PayTask;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.Map;
+
+public class AliModule extends ReactContextBaseJavaModule {
+    private static final int SDK_PAY_FLAG = 1;
+
+    public AliModule(ReactApplicationContext reactApplicationContext) {
+        super(reactApplicationContext);
+    }
+
+    @Override
+    public String getName() {
+        return "AliModule";
+    }
+
+    @ReactMethod
+    public void pay(final String orderInfo, final Promise promise) {
+
+        Runnable payRunnable = new Runnable() {
+            @Override
+            public void run() {
+                PayTask alipay = new PayTask(getCurrentActivity());
+                Map<String, String> result = alipay.payV2(orderInfo, true);
+                //WritableMap map = Arguments.createMap();
+                promise.resolve(1);
+            }
+        };
+
+        Thread payThread = new Thread(payRunnable);
+        payThread.start();
+    }
+}
--- jsx/config.js
+++ jsx/config.js
@@ -1,6 +1,6 @@
 export default {
     we:{
         // api: '123.57.4.169:55556/niceGirl'
-        api: '192.168.1.104:8000/niceGirl'
+        api: '192.168.3.43:8000/niceGirl'
     }
 };
